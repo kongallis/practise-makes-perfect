2492e3a77c9b1563cb9b6658392eec65dbfad669
Konstantinos Gallis <kongallis@gmail.com>
2022-12-02 18:45:07 +0200

    Solution to Collatz Conjecture (JavaScript)
    Link:
    https://exercism.org/tracks/javascript/exercises/collatz-conjecture

---
+++ exercism/Collatz Conjecture/index.js
@@ -0,0 +1,16 @@
+export const steps = (n) => {
+  let steps = 0
+  while (n !== 1) {
+    if (n <= 0) throw new Error('Only positive numbers are allowed')
+
+    if (n % 2 === 0) {
+      n = n / 2
+    } else {
+      n = (n * 3) + 1
+    }
+
+    steps++
+  }
+
+  return steps
+}
